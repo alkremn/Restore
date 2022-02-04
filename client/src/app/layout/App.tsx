517ee3a93cc2234541cd92353686b1c419af4953
Alexey Kremnev <AlexK@zyto.com>
2022-02-03 23:14:54 -0700

    End of secion 4

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -3,6 +3,11 @@ import { Container, createTheme, CssBaseline } from '@mui/material';
 import { useState } from 'react';
 import Catalag from '../../features/catalog/Catalag';
 import Header from './Header';
+import HomePage from './../../features/home/HomePage';
+import { Route, Switch } from 'react-router-dom';
+import ProductDetails from './../../features/catalog/ProductDetails';
+import AboutPage from './../../features/about/AboutPage';
+import ContactPage from './../../features/contact/ContactPage';
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -26,7 +31,11 @@ function App() {
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
-        <Catalag />
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/catalog' component={Catalag} />
+        <Route path='/catalog/:id' component={ProductDetails} />
+        <Route path='/about' component={AboutPage} />
+        <Route path='/contact' component={ContactPage} />
       </Container>
     </ThemeProvider>
   );
