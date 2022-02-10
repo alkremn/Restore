61c8a5433f42818381f594ac48de8c9ad8338a9d
Alexey Kremnev <AlexK@zyto.com>
2022-02-10 00:28:05 -0700

    End of section 5

--- client/src/features/catalog/Catalag.tsx
+++ client/src/features/catalog/Catalag.tsx
@@ -1,15 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { Product } from '../../app/models/product';
 import ProductList from './ProductList';
+import agent from './../../app/api/agent';
+import LoadingComponent from '../../app/layout/LoadingComponent';
 
 export default function Catalag() {
   const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    fetch('http://localhost:5000/api/products')
-      .then(response => response.json())
-      .then(data => setProducts(data));
+    setLoading(true);
+    agent.Catalog.list()
+      .then(products => setProducts(products))
+      .catch(error => console.log(error))
+      .finally(() => setLoading(false));
   }, []);
+
+  if (loading) return <LoadingComponent message='Loading Products' />;
 
   return (
     <>
